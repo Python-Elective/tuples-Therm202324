4ab1c653bd4da9c29bce112bad2707f8acfaf510
Therm <Therm.mu2025@kmids.ac.th>
2023-12-04 09:03:18 +0700

    Done 6

--- Therm202324.G1103ElecPy.Exercise1.py
+++ Therm202324.G1103ElecPy.Exercise1.py
@@ -15,22 +15,17 @@ skip_tuples(('I', 'am', 'a', 'test', 'tuple'))
 #     new = input_tuple[::2]
 #     return new
 # # return input_tuple[::2]
-
 # def main():
-
 #     # Test cases
 #     input_tuple_1 = ('I', 'am', 'a', 'test', 'tuple')
 #     output_tuple_1 = skip_tuples(input_tuple_1)
 #     print(f"Input: {input_tuple_1}\nOutput: {output_tuple_1}\n")
-
 #     input_tuple_2 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 #     output_tuple_2 = skip_tuples(input_tuple_2)
 #     print(f"Input: {input_tuple_2}\nOutput: {output_tuple_2}\n")
-
 #     input_tuple_3 = ('a', 'b', 'c', 'd', 'e', 'f')
 #     output_tuple_3 = skip_tuples(input_tuple_3)
 #     print(f"Input: {input_tuple_3}\nOutput: {output_tuple_3}\n")
-
 # if __name__ == "__main__":
 #         main()
 
