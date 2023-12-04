89dbd924d0b0c357d292dc30138dff30db85d39d
Therm <Therm.mu2025@kmids.ac.th>
2023-12-04 09:00:45 +0700

    Done 6

--- Therm202324.G1103ElecPy.Exercise1.py
+++ Therm202324.G1103ElecPy.Exercise1.py
@@ -11,25 +11,46 @@ Example:
 skip_tuples(('I', 'am', 'a', 'test', 'tuple'))
 ('I', 'a', 'tuple')
 """
-def skip_tuples(input_tuple):
-    new = input_tuple[::2]
-    return new
-# return input_tuple[::2]
+# def skip_tuples(input_tuple):
+#     new = input_tuple[::2]
+#     return new
+# # return input_tuple[::2]
+
+# def main():
+
+#     # Test cases
+#     input_tuple_1 = ('I', 'am', 'a', 'test', 'tuple')
+#     output_tuple_1 = skip_tuples(input_tuple_1)
+#     print(f"Input: {input_tuple_1}\nOutput: {output_tuple_1}\n")
+
+#     input_tuple_2 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+#     output_tuple_2 = skip_tuples(input_tuple_2)
+#     print(f"Input: {input_tuple_2}\nOutput: {output_tuple_2}\n")
+
+#     input_tuple_3 = ('a', 'b', 'c', 'd', 'e', 'f')
+#     output_tuple_3 = skip_tuples(input_tuple_3)
+#     print(f"Input: {input_tuple_3}\nOutput: {output_tuple_3}\n")
+
+# if __name__ == "__main__":
+#         main()
 
 def main():
-
     # Test cases
     input_tuple_1 = ('I', 'am', 'a', 'test', 'tuple')
     output_tuple_1 = skip_tuples(input_tuple_1)
-    print(f"Input: {input_tuple_1}\nOutput: {output_tuple_1}\n")
+    print(f"{output_tuple_1}")
 
     input_tuple_2 = (1, 2, 3, 4, 5, 6, 7, 8, 9)
     output_tuple_2 = skip_tuples(input_tuple_2)
-    print(f"Input: {input_tuple_2}\nOutput: {output_tuple_2}\n")
+    print(f"{output_tuple_2}")
 
     input_tuple_3 = ('a', 'b', 'c', 'd', 'e', 'f')
     output_tuple_3 = skip_tuples(input_tuple_3)
-    print(f"Input: {input_tuple_3}\nOutput: {output_tuple_3}\n")
+    print(f"{output_tuple_3}")
+
+def skip_tuples(t):
+    new = t[::2]
+    return new
 
 if __name__ == "__main__":
         main()
